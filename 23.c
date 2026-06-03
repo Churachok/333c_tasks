25c4f79d917a76c563219f4f80c8faa3e7e6e398
Churachok <kirillkonoplev510@gmail.com>
2026-06-03 19:40:42 +0700

    add 23 task

---
+++ 23.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    bool flag = true;
+    
+    if (flag) {
+        printf("Флаг истинен\n");
+    } else {
+        printf("Флаг ложен\n");
+    }
+    return 0;
+}
